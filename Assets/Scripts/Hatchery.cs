3f67f802ea2b33aa0ed9cb13d42b94e6d05664a1
Jan Stachurski <jan.stachurski@outlook.com>
2024-02-24 17:56:48 +0100

    mrówkas spwaning

--- Assets/Scripts/Hatchery.cs
+++ Assets/Scripts/Hatchery.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hatchery : MonoBehaviour
 {
+    [SerializeField] private GameObject WorkerPrefab;
+    [SerializeField] private float Cooldown;
+    private float LastSpawned;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LastSpawned = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - LastSpawned > Cooldown)
+        {
+            LastSpawned = Time.time;
+            SpawnAnt();
+        }
+    }
+
+    private void SpawnAnt()
+    {
+        GameObject.Instantiate(WorkerPrefab, gameObject.transform);
     }
 }
--- Assets/Scripts/WorkerScript.cs
+++ Assets/Scripts/WorkerScript.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class WorkerScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(0, 5);
     }
 }
