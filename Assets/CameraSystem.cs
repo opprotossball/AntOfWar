d5dc305ac54c4708b106a85c8b87c376d99a8acc
Paterson11 <kampix11@gmail.com>
2024-02-25 02:57:15 +0100

    CameraMovement

---
+++ Assets/CameraSystem.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+
+public class CameraSystem : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    [SerializeField] private bool useEdgeScrolling = false;
+    [SerializeField] private bool useDragPan = false;
+    [SerializeField] private float fieldOfViewMax = 50;
+    [SerializeField] private float fieldOfViewMin = 10;
+    private bool dragPanMoveActive;
+    private Vector2 lastMousePosition;
+    private float targetFieldOfView = 50;
+
+    [SerializeField]
+    private void Update()
+    {
+
+        HandleCameraZoom();
+
+    }
+
+    private void HandleCameraZoom()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            targetFieldOfView += 5;
+        }
+
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFieldOfView -= 5;
+        }
+
+        targetFieldOfView = Mathf.Clamp(targetFieldOfView, fieldOfViewMax, fieldOfViewMin);
+        float zoomSpeed = 10f;
+
+
+        cinemachineVirtualCamera.m_Lens.FieldOfView = Mathf.Lerp(cinemachineVirtualCamera.m_Lens.FieldOfView, targetFieldOfView, Time.deltaTime * zoomSpeed);
+    }
+
+
+
+}
+
